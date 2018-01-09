d8608e0dc665f2b36ebcb1705fc8f7c65344cef5
Sergey Pchelintsev <linz.sergey@gmail.com>
2018-01-09 22:15:50 +0200

    Fix payload submit for remote form with body-less methods

--- src/ujs/form.js
+++ src/ujs/form.js
@@ -1,9 +1,22 @@
 import xhr from "./xhr";
 import dom from "./dom";
 
+const QUERY_METHODS = ['GET', 'HEAD'];
+const toQuery = data => Array.from(data).map(e => encodeURIComponent(e[0]) + "=" + encodeURIComponent(e[1])).join('&');
+
 export default function(form) {
   if(dom.isRemote(form)) {
-    xhr(form.action, form.method, { target: form, data: new FormData(form) });
+    let url;
+    let data = new FormData(form);
+    let options = { target: form };
+    if(QUERY_METHODS.indexOf(form.method.toUpperCase()) === -1) {
+      url = form.action;
+      options.data = data;
+    } else {
+      url = form.action + (form.action.indexOf('?') === -1 ? '?' : '&') + toQuery(data);
+    }
+
+    xhr(url, form.method, options);
     return true;
   }
   return false;
